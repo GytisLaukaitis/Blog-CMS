3e08db5a68c7df81b5792e0ef6ec694afb9817b4
Gytis Gytis <gytislaukaitis@gmail.com>
2020-10-22 10:26:42 +0300

    All blogs

--- resources/views/website/template/master.blade.php
+++ resources/views/website/template/master.blade.php
@@ -54,7 +54,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">Start Blogging</a>
+        <a class="navbar-brand" href="{{ url('/') }}">All Blogs</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
                 data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
                 aria-label="Toggle navigation">
